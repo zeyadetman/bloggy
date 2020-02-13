c485ca6de7e2404ce88679fa0c5c23c514031d82
Zeyad Etman <zeyadetman@gmail.com>
2020-02-13 15:06:37 +0200

    Update configs.js

--- configs.js
+++ configs.js
@@ -2,10 +2,10 @@ module.exports = {
   postsPerPage: 10,
   primary: 'rgb(36, 41, 46)',
   siteMetadata: {
-    title: 'Bloggy',
+    title: {secrets.TITLE},
     description: 'Blog of mine',
     author: {
-      name: 'Zeyad Etman',
+      name: {secrets.AUTHOR_NAME},
       avatar: 'https://pbs.twimg.com/profile_images/1166663145118732288/BtHLImve_400x400.jpg',
       title: 'Software Engineer',
       twitter: 'zeyadetman',
